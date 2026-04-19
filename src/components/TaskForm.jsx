b4e617a0643c741a46fedbc1ffd29c1a2846b159
Arvin Raed <139760390+ghostm0nk@users.noreply.github.com>
2026-04-19 19:12:48 +0600

    Update src/components/TaskForm.jsx

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -10,17 +10,23 @@ const TaskForm = ({ onAddTask }) => {
   };
 
   return (
-    <form onSubmit={handleAddTask} className="p-2 border-b border-gray-200">
+    <form onSubmit={handleAddTask} className="p-4 border-b border-gray-200">
+      <label
+        htmlFor="task"
+        className="block text-sm font-medium text-gray-700 mb-2"
+      >
+        Add new task
+      </label>
       <input
         type="text"
+        id="task"
         value={taskText}
         onChange={(e) => setTaskText(e.target.value)}
-        placeholder="Add new task"
-        className="p-2 border border-gray-200 rounded-md"
+        className="block w-full p-2 border border-gray-200 rounded-md text-gray-700 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
       />
       <button
         type="submit"
-        className="bg-green-500 p-2 text-white rounded-md hover:bg-green-600"
+        className="mt-4 bg-blue-500 p-2 text-white rounded-md hover:bg-blue-600"
       >
         Add Task
       </button>
