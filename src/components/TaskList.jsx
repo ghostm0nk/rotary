cad8d0ff6baf30b564bca10838abc28036a37bc2
Arvin Raed <139760390+ghostm0nk@users.noreply.github.com>
2026-04-19 19:12:46 +0600

    Update src/components/TaskList.jsx

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,7 +2,7 @@ import TaskItem from './TaskItem';
 
 const TaskList = ({ tasks, onUpdateTask, onDeleteTask }) => {
   return (
-    <ul>
+    <ul className="divide-y divide-gray-200">
       {tasks.map((task) => (
         <TaskItem
           key={task.id}
